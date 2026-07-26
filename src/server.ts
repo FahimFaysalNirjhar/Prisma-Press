2fc5892477e6271272f3960ad8c8de686887e9b2
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-07-26 15:35:03 +0600

    env configer completed

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from "./app";
+import config from "./config";
 import { prisma } from "./lib/prisma";
 
-const port = process.env.PORT || 5000;
+const port = config.port;
 
 async function main() {
   try {

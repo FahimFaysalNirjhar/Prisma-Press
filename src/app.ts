d2739364e30d5b2b64e2957df47acc5ac94eaf81
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-07-26 16:54:04 +0600

    model user and profile created

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import config from "./config";
 
 const app: Application = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
+
+app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true }));
 

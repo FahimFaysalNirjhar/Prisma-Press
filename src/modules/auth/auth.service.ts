b3be65aa59851aee652639f0b79a955bb307165c
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-07-28 15:08:37 +0600

    accessToken, refreshToken created successfully

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
 import { ILogingUser } from "./auth.interface";
+import jwt from "jsonwebtoken";
+import config from "../../config";
 
 const loginUserIntoDB = async (payload: ILogingUser) => {
   const { email, password } = payload;
@@ -13,7 +15,21 @@ const loginUserIntoDB = async (payload: ILogingUser) => {
   if (!isPasswordMatched) {
     throw new Error("Password is incorrect");
   }
-  return user;
+
+  const jwtPayload = {
+    id: user?.id,
+    email: user?.email,
+    name: user?.name,
+  };
+
+  const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret, {
+    expiresIn: "1d",
+  });
+
+  const refreshToken = jwt.sign(jwtPayload, config.jwt_refesh_secret, {
+    expiresIn: "7d",
+  });
+  return { accessToken, refreshToken };
 };
 
 export const authService = { loginUserIntoDB };
